54a2deb2fb28042afa79db86a52ac3b2f08cce62
Randolio <103154494+Randolio@users.noreply.github.com>
2023-08-09 12:23:12 +0100

    Revert "Added a configurable tax system"

--- config.lua
+++ config.lua
@@ -4,5 +4,3 @@ Config.Ped = {
     model = `a_f_y_business_02`,
     coords = vec4(241.54, 227.0, 105.29, 150.23), -- Default Pacific Bank.
 }
-
-Config.TaxRate = 0.10 -- 10% tax rate | If you dont want tax, set this to 0.
