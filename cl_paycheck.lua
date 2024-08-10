91b46988584461779e787e221b84729a97a46daf
Randolio <103154494+Randolio@users.noreply.github.com>
2024-08-10 17:35:16 +0100

    refactor(client): qb/ox target + lib.playAnim

--- cl_paycheck.lua
+++ cl_paycheck.lua
@@ -1,5 +1,19 @@
 local Config = lib.require('config')
 local PC_PED, initZone
+local oxtarget = GetResourceState('ox_target') == 'started'
+
+local function targetLocalEntity(entity, options, distance)
+    if oxtarget then
+        for _, option in ipairs(options) do
+            option.distance = distance
+            option.onSelect = option.action
+            option.action = nil
+        end
+        exports.ox_target:addLocalEntity(entity, options)
+    else
+        exports['qb-target']:AddTargetEntity(entity, { options = options, distance = distance })
+    end
+end
 
 local function InputWithdraw(amount)
     local response = lib.inputDialog('Withdrawal', {
@@ -12,13 +26,16 @@ local function InputWithdraw(amount)
     if not response then return end
     local inputAmt = response[1]
     local accountType = response[2]
-    if inputAmt < 1 then 
-        return DoNotification('Amount needs to be more than 0.', 'error') 
+    
+    if inputAmt < 1 then return DoNotification('Amount needs to be more than 0.', 'error') end
+    if inputAmt > amount then return DoNotification('You don\'t have this much in your paycheck..', 'error') end
+
+    local success = lib.callback.await('randol_paycheck:server:withdraw', false, inputAmt, accountType)
+    if success then
+        lib.playAnim(cache.ped, 'friends@laf@ig_5', 'nephew', 8.0, -8.0, -1, 49, 0, false, false, false)
+        Wait(2000)
+        ClearPedTasks(cache.ped)
     end
-    if inputAmt > amount then 
-        return DoNotification('You don\'t have this much in your paycheck..', 'error')
-    end
-    TriggerServerEvent('randol_paycheck:server:withdraw', inputAmt, accountType)
 end
 
 local function viewPaycheck()
@@ -42,13 +59,17 @@ end
 
 local function removePed()
     if not DoesEntityExist(PC_PED) then return end
+    if oxtarget then
+        exports.ox_target:removeLocalEntity(PC_PED, 'View Paycheck')
+    else
+        exports['qb-target']:RemoveTargetEntity(PC_PED, 'View Paycheck')
+    end
     DeleteEntity(PC_PED)
-    exports['qb-target']:RemoveTargetEntity(PC_PED, 'View Paycheck')
     PC_PED = nil
 end
 
 local function spawnPed()
-    lib.requestModel(Config.model)
+    lib.requestModel(Config.model, 10000)
     PC_PED = CreatePed(0, Config.model, Config.coords, false, false)
     SetEntityAsMissionEntity(PC_PED)
     SetPedFleeAttributes(PC_PED, 0, 0)
@@ -57,43 +78,31 @@ local function spawnPed()
     FreezeEntityPosition(PC_PED, true)
     SetPedDefaultComponentVariation(PC_PED)
     SetModelAsNoLongerNeeded(Config.model)
-    lib.requestAnimDict('mp_prison_break')
-    TaskPlayAnim(PC_PED, 'mp_prison_break', 'hack_loop', 8.0, -8.0, -1, 1, 0.0, 0, 0, 0)
-    RemoveAnimDict('mp_prison_break')
-    exports['qb-target']:AddTargetEntity(PC_PED, {
-        options = {
-            {
-                icon = 'fa-solid fa-money-check-dollar', 
-                label = 'View Paycheck',
-                action = function()
-                    lib.requestAnimDict('friends@laf@ig_5')
-                    TaskPlayAnim(cache.ped, 'friends@laf@ig_5', 'nephew', 8.0, -8.0, -1, 49, 0, false, false, false)
-                    RemoveAnimDict('friends@laf@ig_5')
-                    if lib.progressCircle({
-                        duration = 2500,
-                        position = 'bottom',
-                        label = 'Viewing paycheck..',
-                        useWhileDead = true,
-                        canCancel = false,
-                        disable = { move = true, car = true, mouse = false, combat = true, },
-                    }) then
-                        ClearPedTasks(cache.ped)
-                        viewPaycheck()
-                    end
-                end,
-            },
+    lib.playAnim(PC_PED, 'mp_prison_break', 'hack_loop', 8.0, -8.0, -1, 1, 0.0, 0, 0, 0)
+    targetLocalEntity(PC_PED, {
+        {
+            icon = 'fa-solid fa-money-check-dollar', 
+            label = 'View Paycheck',
+            action = function()
+                lib.playAnim(cache.ped, 'friends@laf@ig_5', 'nephew', 8.0, -8.0, -1, 49, 0, false, false, false)
+                if lib.progressCircle({
+                    duration = 2500,
+                    position = 'bottom',
+                    label = 'Viewing paycheck..',
+                    useWhileDead = true,
+                    canCancel = false,
+                    disable = { move = true, car = true, mouse = false, combat = true, },
+                }) then
+                    ClearPedTasks(cache.ped)
+                    viewPaycheck()
+                end
+            end,
         },
-        distance = 4.5,
-    })
+    }, 4.5)
 end
 
 local function paycheckZone()
-    initZone = lib.points.new({
-        coords = Config.coords.xyz,
-        distance = 50,
-        onEnter = spawnPed,
-        onExit = removePed,
-    })
+    initZone = lib.points.new({ coords = Config.coords.xyz, distance = 50, onEnter = spawnPed, onExit = removePed, })
 end
 
 function OnPlayerLoaded()
