babbdc62a7f894357a858bbaea51cb20dcc8c3fe
Randolio <103154494+Randolio@users.noreply.github.com>
2024-08-10 17:35:55 +0100

    refactor(server): use callback.

--- sv_paycheck.lua
+++ sv_paycheck.lua
@@ -7,7 +7,6 @@ local function AddToPaycheck(cid, amount)
         ON DUPLICATE KEY UPDATE amount = amount + ?
     ]], {cid, amount, amount})
 
-
     local result = MySQL.single.await('SELECT amount FROM paychecks WHERE citizenid = ?', {cid})
     if not result then return end
     
@@ -16,16 +15,19 @@ local function AddToPaycheck(cid, amount)
         DoNotification(src, ('$%s was added to your paycheck. New Total: $%s'):format(amount, result.amount), 'success')
     end
 end
-exports("AddToPaycheck", AddToPaycheck)
+exports('AddToPaycheck', AddToPaycheck)
 
-RegisterNetEvent('randol_paycheck:server:withdraw', function(amount, accountType)
+lib.callback.register('randol_paycheck:server:withdraw', function(source, amount, accountType)
     local src = source
     local Player = GetPlayer(src)
     local cid = GetPlyIdentifier(Player)
     local result = MySQL.rawExecute.await('SELECT amount FROM paychecks WHERE citizenid = ?', {cid})
-    if not result[1].amount then return end
+    if not result[1].amount then return false end
 
-    if tonumber(result[1].amount) < amount then DoNotification(src, 'You dont have this much in your paycheck.', 'error') return end
+    if tonumber(result[1].amount) < amount then 
+        DoNotification(src, 'You dont have this much in your paycheck.', 'error')
+        return false
+    end
 
     result[1].amount -= amount
     MySQL.update.await('UPDATE paychecks SET amount = ? WHERE citizenid = ?', {result[1].amount, cid})
@@ -37,10 +39,7 @@ RegisterNetEvent('randol_paycheck:server:withdraw', function(amount, accountType
         AddMoney(Player, 'bank', amount)
         DoNotification(src, ('You withdrew $%s from your paycheck into your bank account.'):format(amount), 'success')
     end
-
-    TaskPlayAnim(GetPlayerPed(src), 'friends@laf@ig_5', 'nephew', 8.0, -8.0, -1, 49, 0, false, false, false)
-    Wait(2000)
-    ClearPedTasks(GetPlayerPed(src))
+    return true
 end)
 
 lib.callback.register('randol_paycheck:server:checkPaycheck', function(source)
@@ -59,11 +58,6 @@ end)
 
 AddEventHandler('onServerResourceStart', function(resource)
     if resource == GetCurrentResourceName() then
-        MySQL.query([=[
-            CREATE TABLE IF NOT EXISTS paychecks (
-            citizenid varchar(100) NOT NULL,
-            amount varchar(50) DEFAULT NULL,
-            PRIMARY KEY (citizenid));
-        ]=])
+        MySQL.query([=[ CREATE TABLE IF NOT EXISTS paychecks ( citizenid varchar(100) NOT NULL, amount varchar(50) DEFAULT NULL, PRIMARY KEY (citizenid)); ]=])
     end
 end)
