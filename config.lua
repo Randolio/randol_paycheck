e3082e6503dd4b9893e17635ec3190433bf78132
John Dowe <mrjohndowe@outlook.com>
2024-08-09 23:56:49 -0600

    Changed the config to a proper config for multiple locations

--- config.lua
+++ config.lua
@@ -1,4 +1,7 @@
-return {
+Config = {
     model = `a_f_y_business_02`,
-    coords = vec4(241.54, 227.0, 105.29, 150.23), -- Default Pacific Bank.
+    locations = {
+        [1] = { coords = vec4(241.54, 227.0, 105.29, 150.23)}, -- Default Pacific Bank.
+        [2] = { coords = vec4(150.732467651367, -1042.40209960938, 28.5465145111084, 341.68)}, -- Legion Square Bank
+    },
 }
