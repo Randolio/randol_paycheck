2893d88872f030adcd2a2a2a4f9ff50ad074a8e4
John Dowe <mrjohndowe@outlook.com>
2024-08-09 23:57:14 -0600

    Changed the ability to add more than one location

--- cl_paycheck.lua
+++ cl_paycheck.lua
@@ -1,4 +1,3 @@
-local Config = lib.require('config')
 local PC_PED, initZone
 
 local function InputWithdraw(amount)
@@ -48,52 +47,56 @@ local function removePed()
 end
 
 local function spawnPed()
-    lib.requestModel(Config.model)
-    PC_PED = CreatePed(0, Config.model, Config.coords, false, false)
-    SetEntityAsMissionEntity(PC_PED)
-    SetPedFleeAttributes(PC_PED, 0, 0)
-    SetBlockingOfNonTemporaryEvents(PC_PED, true)
-    SetEntityInvincible(PC_PED, true)
-    FreezeEntityPosition(PC_PED, true)
-    SetPedDefaultComponentVariation(PC_PED)
-    SetModelAsNoLongerNeeded(Config.model)
-    lib.requestAnimDict('mp_prison_break')
-    TaskPlayAnim(PC_PED, 'mp_prison_break', 'hack_loop', 8.0, -8.0, -1, 1, 0.0, 0, 0, 0)
-    RemoveAnimDict('mp_prison_break')
-    exports['qb-target']:AddTargetEntity(PC_PED, {
-        options = {
-            {
-                icon = 'fa-solid fa-money-check-dollar', 
-                label = 'View Paycheck',
-                action = function()
-                    lib.requestAnimDict('friends@laf@ig_5')
-                    TaskPlayAnim(cache.ped, 'friends@laf@ig_5', 'nephew', 8.0, -8.0, -1, 49, 0, false, false, false)
-                    RemoveAnimDict('friends@laf@ig_5')
-                    if lib.progressCircle({
-                        duration = 2500,
-                        position = 'bottom',
-                        label = 'Viewing paycheck..',
-                        useWhileDead = true,
-                        canCancel = false,
-                        disable = { move = true, car = true, mouse = false, combat = true, },
-                    }) then
-                        ClearPedTasks(cache.ped)
-                        viewPaycheck()
-                    end
-                end,
+    for k in pairs(Config.locations) do 
+        lib.requestModel(Config.model)
+        PC_PED = CreatePed(0, Config.model, Config.locations[k].coords, false, false)
+        SetEntityAsMissionEntity(PC_PED)
+        SetPedFleeAttributes(PC_PED, 0, 0)
+        SetBlockingOfNonTemporaryEvents(PC_PED, true)
+        SetEntityInvincible(PC_PED, true)
+        FreezeEntityPosition(PC_PED, true)
+        SetPedDefaultComponentVariation(PC_PED)
+        SetModelAsNoLongerNeeded(Config.model)
+        lib.requestAnimDict('mp_prison_break')
+        TaskPlayAnim(PC_PED, 'mp_prison_break', 'hack_loop', 8.0, -8.0, -1, 1, 0.0, 0, 0, 0)
+        RemoveAnimDict('mp_prison_break')
+        exports['qb-target']:AddTargetEntity(PC_PED, {
+            options = {
+                {
+                    icon = 'fa-solid fa-money-check-dollar', 
+                    label = 'View Paycheck',
+                    action = function()
+                        lib.requestAnimDict('friends@laf@ig_5')
+                        TaskPlayAnim(cache.ped, 'friends@laf@ig_5', 'nephew', 8.0, -8.0, -1, 49, 0, false, false, false)
+                        RemoveAnimDict('friends@laf@ig_5')
+                        if lib.progressCircle({
+                            duration = 2500,
+                            position = 'bottom',
+                            label = 'Viewing paycheck..',
+                            useWhileDead = true,
+                            canCancel = false,
+                            disable = { move = true, car = true, mouse = false, combat = true, },
+                        }) then
+                            ClearPedTasks(cache.ped)
+                            viewPaycheck()
+                        end
+                    end,
+                },
             },
-        },
-        distance = 4.5,
-    })
+            distance = 4.5,
+        })
+    end
 end
 
 local function paycheckZone()
-    initZone = lib.points.new({
-        coords = Config.coords.xyz,
-        distance = 50,
-        onEnter = spawnPed,
-        onExit = removePed,
-    })
+    for k in pairs(Config.locations) do 
+        initZone = lib.points.new({
+            coords = Config.locations[k].coords.xyz,
+            distance = 50,
+            onEnter = spawnPed,
+            onExit = removePed,
+        })
+    end
 end
 
 function OnPlayerLoaded()
